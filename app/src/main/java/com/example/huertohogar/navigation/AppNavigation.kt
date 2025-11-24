526f7a4ae9f5956dc825e2de24d1c91e38063b8f
diegocardenasg <die.cardenasg@duocuc.cl>
2025-11-24 05:19:08 -0300

    Integracion de Navegaciones de Screen

---
+++ app/src/main/java/com/example/huertohogar/navigation/AppNavigation.kt
@@ -0,0 +1,13 @@
+package com.example.huertohogar.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(navController = navController, startDestination = "Screen")
+
+}
